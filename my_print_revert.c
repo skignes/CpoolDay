f1b8634478271c18300b0a17c1afdba523d756f8
Skignes <116216779+skignes@users.noreply.github.com>
2025-06-02 20:04:07 +0200

    asdlkfjsa;ldkfj

--- my_print_revert.c
+++ my_print_revert.c
@@ -1,11 +1,12 @@
+#include <string.h>
+
 void print_char(char c);
 
 void my_print_revert(char *to_revert)
 {
     int index = 0;
+    int len = strlen(to_revert);
 
-    while(to_revert[index])
-        index++;
-    for (; index >= 0; index--)
+    for (index = len - 1; index >= 0; index--)
         print_char(to_revert[index]);
 }
